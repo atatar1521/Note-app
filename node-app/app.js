4dd5ba3f27a5155bc963f3c4d694308fc7a8028f
atatar1521 <68011473+atatar1521@users.noreply.github.com>
2020-07-29 23:01:29 +0300

    Add files via upload

---
+++ node-app/app.js
@@ -0,0 +1,68 @@
+
+const chalk = require('chalk');
+const notes= require('./nodes.js');
+const yargs = require('yargs');
+
+//costumize yargs version
+yargs.version('1.1.0');
+//craete add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder:{
+     title:{
+         describe:'Note title',
+         demandOption: true,
+         type: 'string'
+     },
+     body:{
+        describe:'Note body',
+        demandOption: true,
+        type: 'string'
+     }
+    },
+    handler(argv){
+   notes.addNote(argv.title,argv.body);
+    }
+})
+
+//create remove command
+yargs.command({
+    command :'remove',
+    describe: 'Remove a note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title);
+         }
+     })
+//create List command
+yargs.command({
+    command :'list',
+    describe: 'Listing a note',
+    handler(argv){
+    notes.listNotes();
+    }
+})
+//create read command
+yargs.command({
+    command :'read',
+    describe: 'reading a note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+       notes.readNote(argv.title);
+    }
+})
+
+yargs.parse();
